f8e27b63dd31b10b77e80ca3aa08972aec64d3db
Tim <tim.sigl@hotmail.de>
2022-01-26 08:51:17 +0100

    installed packges locally

--- clients/e-shop-demo/e-shop-browser-extension/signer.js
+++ clients/e-shop-demo/e-shop-browser-extension/signer.js
@@ -16,6 +16,10 @@ injector.addEventListener('click', async () => {
 
 function injectSignedNonce() {
     chrome.storage.sync.get("secretKey", ({secretKey}) => {
+        var backendUrl = "http://localhost:3005"
+        var id = 2
+        var result = axios.get(`backendUrl/nonce/${id}`)
+        console.log(result)
         document.getElementById('signedNonce').value = secretKey
     })
 }
