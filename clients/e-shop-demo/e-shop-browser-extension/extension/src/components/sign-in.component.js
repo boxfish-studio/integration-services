6c5d92401ab69785c4c4fec66baa81dc32cf949a
Tim <tim.sigl@hotmail.de>
2022-01-26 14:12:35 +0100

    added react project

---
+++ clients/e-shop-demo/e-shop-browser-extension/extension/src/components/sign-in.component.js
@@ -0,0 +1,13 @@
+const SignIn = () => {
+
+    return(
+        <>
+        <input type="text" placeholder="Identity ID"></input>
+        <input type="password" placeholder="Secret key"></input>
+        <button>Sign in</button>
+        </>
+        
+    )
+};
+
+export default SignIn;
