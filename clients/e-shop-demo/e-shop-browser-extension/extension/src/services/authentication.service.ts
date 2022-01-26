756b20c36be64e7032b67712b73c3b7c9ab065be
Tim <tim.sigl@hotmail.de>
2022-01-26 14:11:58 +0100

    added react project

---
+++ clients/e-shop-demo/e-shop-browser-extension/extension/src/services/authentication.service.ts
@@ -0,0 +1,18 @@
+export const generateNonce = async (id: any): Promise<string> => {
+	const url = `${process.env.REACT_APP_E_SHOP_BACKEND_URL}/nonce/${id}`;
+	const response = await client.get(url);
+	return response.data.nonce;
+};
+
+export const authenticate = async (signedNonce: string, id: string): Promise<boolean> => {
+	try {
+		const url = `${process.env.REACT_APP_E_SHOP_BACKEND_URL}/authenticate/${id}`;
+		const response = await client.post(url, JSON.stringify({ signedNonce }));
+		const jwt = response.data.jwt;
+		setAuthHeader(jwt);
+		return true;
+	} catch (error) {
+		console.log(error);
+		return false;
+	}
+};
