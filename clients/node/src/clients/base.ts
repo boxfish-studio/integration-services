ad089b35f90344da0278406e37c15e3fe67d68c5
Ruben Alvarez <ralvarez@boxfish.studio>
2022-02-01 12:25:38 +0100

    console log

--- clients/node/src/clients/base.ts
+++ clients/node/src/clients/base.ts
@@ -56,7 +56,7 @@ export abstract class BaseClient {
     const data = encoder.encode(nonce);
     console.log("data", data);
     console.log("sha256", sha256(nonce));
-    console.log("sha256 hex", sha256(nonce));
+    console.log("sha256 toString", sha256(nonce).toString());
 
     // const hash = crypto.createHash('sha256').update(data).digest().toString('hex');
     // console.log("crypto.createHash('sha256').update(data).digest().toString('hex')", hash);
